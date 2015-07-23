2d4a0098ef863146eaf72647b76f08c048a0be64
yui <yui@7c2116fe-53d0-4aec-ab59-80607f05abc0>
2015-07-23 03:01:06 +0000

    win9x: refactor sound manager  svn merge -r 1653:1654 https://amethyst.yui.ne.jp/svn-dev/pc98/np2/branches/yui/reform_ui  svn merge -r 1759:1761 https://amethyst.yui.ne.jp/svn-dev/pc98/np2/branches/yui/reform_ui

--- win9x/soundmng.h
+++ win9x/soundmng.h
@@ -1,23 +1,23 @@
+/**
+ * @file	soundmng.h
+ * @brief	サウンド マネージャの宣言およびインターフェイスの定義をします
+ */
 
-enum {
-	SOUND_PCMSEEK		= 0,
-	SOUND_PCMSEEK1		= 1,
-
-	SOUND_MAXPCM
-};
-
-enum {
-	SNDPROC_MASTER		= 0,
-	SNDPROC_MAIN,
-	SNDPROC_TOOL,
-	SNDPROC_SUBWIND
-};
-
-
+#pragma once
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
+
+/**
+ * PCM 番号
+ */
+typedef enum
+{
+	SOUND_PCMSEEK		= 0,		/*!< ヘッド移動 */
+	SOUND_PCMSEEK1					/*!< 1クラスタ移動 */
+} SoundPCMNumber;
 
 UINT soundmng_create(UINT rate, UINT ms);
 void soundmng_destroy(void);
@@ -25,21 +25,32 @@ void soundmng_reset(void);
 void soundmng_play(void);
 void soundmng_stop(void);
 void soundmng_sync(void);
-void soundmng_setreverse(BOOL reverse);
+void soundmng_setreverse(BOOL bReverse);
 
-BRESULT soundmng_pcmplay(UINT num, BOOL loop);
-void soundmng_pcmstop(UINT num);
-
-void soundmng_enable(UINT proc);
-void soundmng_disable(UINT proc);
+BRESULT soundmng_pcmplay(SoundPCMNumber nNum, BOOL bLoop);
+void soundmng_pcmstop(SoundPCMNumber nNum);
 
 // ---- for windows
 
 BRESULT soundmng_initialize(void);
 void soundmng_deinitialize(void);
 
-void soundmng_pcmload(UINT num, LPCTSTR lpFilename);
-void soundmng_pcmvolume(UINT num, int volume);
+void soundmng_pcmload(SoundPCMNumber nNum, LPCTSTR lpFilename);
+void soundmng_pcmvolume(SoundPCMNumber nNum, int nVolume);
+
+/**
+ * サウンド プロシージャ
+ */
+typedef enum
+{
+	SNDPROC_MASTER		= 0,
+	SNDPROC_MAIN,
+	SNDPROC_TOOL,
+	SNDPROC_SUBWIND
+} SoundProc;
+
+void soundmng_enable(SoundProc nProc);
+void soundmng_disable(SoundProc nProc);
 
 #ifdef __cplusplus
 }
